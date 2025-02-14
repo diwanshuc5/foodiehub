823b945f198b7283fd1e9521f6d3cc13905205ae
Diwanshu Chouragade <diwanshuc5@gmail.com>
2025-02-14 15:48:03 +0530

    Added dotenv, CORS, nodemon, and basic setup

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+require("dotenv").config(); // Load environment variables
 const express = require("express");
-const app = express();
-const PORT = 5000;
+const cors = require("cors");
 
-// Test route
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors()); // Enable CORS
+app.use(express.json()); // Enable JSON request handling
+
 app.get("/", (req, res) => {
     res.send("FoodieHub Server is Running...");
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
